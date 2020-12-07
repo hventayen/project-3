dc51f1985c9df5fe63177e1d4d87293e94b2330e
hventayen <hventayen@csu.fullerton.edu>
2020-12-06 21:11:13 -0800

    finished

--- CPSC 131 Project 3/GateControl.hpp
+++ CPSC 131 Project 3/GateControl.hpp
@@ -17,16 +17,16 @@ typedef size_t GateNumber;
 struct Authorization
 {
 	Authorization() { }
-	
+
 	Authorization(CardNumber number, const string& name, const string& startTime, const string& endTime)
 	: number_(number), name_(name), startTime_(startTime), endTime_(endTime) { }
 
 	CardNumber number_;
 
 	string name_;
-	
+
 	string startTime_;
-	
+
 	string endTime_;
 };
 
@@ -43,13 +43,13 @@ struct Transaction
 	: number_(number), name_(name), date_(date), time_(time), accessAllowed_(accessAllowed) { }
 
 	CardNumber number_;
-	
+
 	string name_;
-	
+
 	string date_;
 
 	string time_;
-	
+
 	bool accessAllowed_;
 };
 
@@ -60,24 +60,24 @@ class	GateControl
 {
 	public:
 		bool accessAllowed(CardNumber number);
-	
+
 		bool addAuthorization(CardNumber number, const string& name, const string& startTime, const string& endTime);
-		
+
 		bool changeAuthorization(CardNumber number, const string& name, const string& startTime, const string& endTime);
-		
+
 		bool deleteAuthorization(CardNumber number);
-		
+
 		void getAllAuthorizations(AuthorizationVector& authorizationVector);
-		
+
 		void getAllTransactions(TransactionVector& transactionVector);
-	
+
 		bool getCardAuthorization(CardNumber number, Authorization& authorization);
 
 		void getCardTransactions(CardNumber number, TransactionVector& transactionVector);
 
 	private:
 		AuthorizationMap authorizationMap_;
-		
+
 		TransactionVector transactionVector_;
 };
 
@@ -98,63 +98,94 @@ extern string gCurrentTime;
 
 bool GateControl::accessAllowed(CardNumber number)
 {
-	
 	//	TO DO
-
+	AuthorizationMapIterator iterator = authorizationMap_.find(number);
+	if (iterator != authorizationMap_.end()){
+		transactionVector_.push_back(Transaction(number,iterator->second.name_,gCurrentDate, gCurrentTime, true));
+		return true;
+	}
+	transactionVector_.push_back(Transaction(number,iterator->second.name_,gCurrentDate,gCurrentTime, false));
+	return false;
 }
 
 
-bool GateControl::addAuthorization(CardNumber number, const string& name, const string& startTime, const string& endTime)
+bool GateControl::addAuthorization(CardNumber number, const string & name, const string & startTime, const string & endTime)
 {
-	
 	//	TO DO
-
+  return authorizationMap_.emplace(pair<CardNumber, Authorization>(number,
+				 Authorization(number, name, startTime, endTime))).second;
 }
 
 
-bool GateControl::changeAuthorization(CardNumber number, const string& name, const string& startTime, const string& endTime)
+bool GateControl::changeAuthorization(CardNumber number, const string & name, const string & startTime, const string & endTime)
 {
-	
 	//	TO DO
-
+  AuthorizationMapIterator iterator = authorizationMap_.find(number);
+  if (iterator != authorizationMap_.end()){
+    iterator->second.name_ = name;
+    iterator->second.startTime_ = startTime;
+    iterator->second.endTime_ = endTime;
+    return true;
+  }
+  return false;
 }
 
 
 bool GateControl::deleteAuthorization(CardNumber number)
 {
-	
 	//	TO DO
-
+	AuthorizationMapIterator iterator = authorizationMap_.find(number);
+	if (iterator != authorizationMap_.end()){
+		authorizationMap_.erase(iterator);
+		return true;
+	}
+	return false;
 }
 
 
 void GateControl::getAllAuthorizations(AuthorizationVector& authorizationVector)
 {
-	
-	//	TO DO	
-
+	//	TO DO
+	authorizationVector.clear();
+	for(AuthorizationMapIterator iterator = authorizationMap_.begin(); iterator != authorizationMap_.end(); iterator++){
+		authorizationVector.push_back(iterator->second);
+	}
 }
 
 
 void GateControl::getAllTransactions(TransactionVector& transactionVector)
 {
-	
 	//	TO DO
-
+	if (!transactionVector_.empty()){
+		transactionVector.assign(transactionVector_.begin(), transactionVector_.end());
+	}
+	else {
+			transactionVector.clear();
+	}
 }
 
 
 bool GateControl::getCardAuthorization(CardNumber number, Authorization& authorization)
 {
-
 	//	TO DO
-
+	AuthorizationMapIterator iterator = authorizationMap_.find(number);
+	if (iterator != authorizationMap_.end()){
+		authorization.number_ = number;
+		authorization.name_ = iterator->second.name_;
+		authorization.startTime_ = iterator->second.startTime_;
+		authorization.endTime_ = iterator->second.endTime_;
+		return true;
+	}
+	return false;
 }
 
 
 void GateControl::getCardTransactions(CardNumber number, TransactionVector& transactionVector)
 {
-	
 	//	TO DO
-
+	for(TransactionVectorIterator iterator = transactionVector_.begin();iterator <= transactionVector_.end(); iterator++){
+		if (iterator->number_ == number){
+			transactionVector.push_back(*iterator);
+		}
+	}
 }
